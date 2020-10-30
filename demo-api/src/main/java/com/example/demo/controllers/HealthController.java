aeabd86a74a5ca55208be81154bd06c49d9e7878
BruceChen <979527682@qq.com>
2020-10-30 14:15:48 +0800

    java打包和部署配置

--- demo-api/src/main/java/com/example/demo/controllers/HealthController.java
+++ demo-api/src/main/java/com/example/demo/controllers/HealthController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author bruce
+ */
 @RestController
 public class HealthController {
     @RequestMapping(value = "/health", method = { RequestMethod.GET, RequestMethod.POST})
